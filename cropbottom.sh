f9f2cb0ce0fc6e86caebf42e8cafe86b3107e0cf
John Comeau <jc@unternet.net>
2026-08-15 00:21:28 +0000

    fixed? will test on laptop

--- cropbottom.sh
+++ cropbottom.sh
@@ -8,8 +8,16 @@ if [ -z "$original" ]; then
 	echo Must specify image to crop >&2
 	exit
 fi
+if [ "$side" = top ]; then
+	gravity=South
+	otherside=bottom
+else
+	gravity=North
+	otherside=top
+fi
 if [ -z "$cropped" ]; then
-	cropped=${original%.*}_top.$suffix
+	cropped=${original%.*}_$otherside.$suffix
 fi
 declare -p
-echo convert $original -gravity South -crop 100%x50%+0+0 $cropped
+echo cropping out $side of $original and saving to $cropped >&2
+convert $original -gravity $gravity -crop 100%x50%+0+0 $cropped
--- croptop.sh
+++ croptop.sh
@@ -1,1 +1,1 @@
-convert somefile.jpg -gravity South -crop 100%x50%+0+0 outfile.jpg
+cropbottom.sh
